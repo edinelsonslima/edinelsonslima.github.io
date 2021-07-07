d8770721ecc5aa6d996f91408ae0d52fc3a822f1
Edinelson Lima <edinelsonlima86@gmail.com>
2021-07-07 20:08:43 -0300

    first commit

---
+++ views/home/home.js
@@ -0,0 +1,14 @@
+const titulo = document.querySelector('#resume');
+
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function (letra, i) {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 75 * i)
+
+    });
+}
+
+typeWrite(titulo);
